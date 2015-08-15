f6c8adbac34f1ca6b4464b2c75e458e895bb6297
Lucas Pereira <luca85perez@gmail.com>
2015-08-15 13:28:07 -0300

    Favicons

--- header.php
+++ header.php
@@ -6,6 +6,14 @@
  *
  */
 ?><!doctype html>
+<!--
+ _                         __     __ __                         _
+| |_   _  ___ __ _ ___    / /    / / \ \    _ __   ___ _ __ ___(_)_ __ __ _
+| | | | |/ __/ _` / __|  | |    / /   | |  | '_ \ / _ \ '__/ _ \ | '__/ _` |
+| | |_| | (_| (_| \__ \ < <    / /     > > | |_) |  __/ | |  __/ | | | (_| |
+|_|\__,_|\___\__,_|___/  | |  /_/     | |  | .__/ \___|_|  \___|_|_|  \__,_|
+                          \_\        /_/   |_|
+-->
 <!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="pt-br"> <![endif]-->
 <!--[if IE 7]>    <html class="no-js ie7 oldie" lang="pt-br"> <![endif]-->
 <!--[if IE 8]>    <html class="no-js ie8 oldie" lang="pt-br"> <![endif]-->
@@ -20,20 +28,26 @@
   <meta name="description" content="" />
   <meta name="keywords" content="" />
 
-  <!-- Facebook Metadata /-->
-  <meta property="fb:page_id" content="" />
-  <meta property="og:image" content="" />
-  <meta property="og:description" content=""/>
-  <meta property="og:title" content=""/>
-
-  <!-- Google+ Metadata /-->
-  <meta itemprop="name" content="">
-  <meta itemprop="description" content="">
-  <meta itemprop="image" content="">
-
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
   <link rel="dns-prefetch" href="https://connect.facebook.net">
+
+  <link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri(); ?>/dist/img/favicons/apple-touch-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri(); ?>/dist/img/favicons/apple-touch-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/dist/img/favicons/apple-touch-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/dist/img/favicons/apple-touch-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/dist/img/favicons/apple-touch-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri(); ?>/dist/img/favicons/apple-touch-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/dist/img/favicons/apple-touch-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri(); ?>/dist/img/favicons/apple-touch-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/dist/img/favicons/apple-touch-icon-180x180.png">
+  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/dist/img/favicons/favicon-32x32.png" sizes="32x32">
+  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/dist/img/favicons/favicon-194x194.png" sizes="194x194">
+  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/dist/img/favicons/favicon-96x96.png" sizes="96x96">
+  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/dist/img/favicons/android-chrome-192x192.png" sizes="192x192">
+  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/dist/img/favicons/favicon-16x16.png" sizes="16x16">
+  <meta name="msapplication-TileColor" content="#000000">
+  <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/dist/img/favicons/mstile-144x144.png">
+  <meta name="theme-color" content="#000000">
 
   <!-- Font loader -->
   <script type="text/javascript">
